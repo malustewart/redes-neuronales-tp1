cde06e23036d960e3b21bb1a072599caea222483
malustewart <mastewart@itba.edu.ar>
2025-08-20 17:44:45 -0300

    multifile structure

---
+++ src/ej4/HH.jl
@@ -0,0 +1,84 @@
+module HH
+
+export default_I, default_V0, default_tmax, C, HH_standard, Conductances, InversionV, MhnParameters, HHParams
+
+default_V0 = -80
+default_I = 1
+default_tmax = 300
+
+g_Na = 120
+g_K = 36
+g_L = 0.3
+V_Na = 50
+V_K = -77
+V_L = -54.4
+C = 1e-6
+
+struct Conductances
+    g_Na
+    g_K
+    g_L
+end
+
+function Conductances()
+    Conductances(g_Na, g_K, g_L)
+end
+
+struct MhnParameters
+    m_inf
+    h_inf
+    n_inf
+    m_tau
+    h_tau
+    n_tau
+end
+
+function MhnParameters(V)
+    a_m = 0.1 * (V + 40) / (1 - exp((-V - 40) / 10))
+    b_m = 4 * exp((-V - 65) / 18)
+    a_h = 0.07 * exp((-V - 65) / 20)
+    b_h = 1 / (1 + exp((-V - 35) / 10))
+    a_n = 0.01 * (V + 55) / (1 - exp((-V - 55) / 10))
+    b_n = 0.125 * exp((-V - 65) / 80)
+
+    m_tau = 1 / (a_m + b_m)
+    h_tau = 1 / (a_h + b_h)
+    n_tau = 1 / (a_n + b_n)
+
+    m_inf = m_tau * a_m
+    h_inf = h_tau * a_h
+    n_inf = n_tau * a_n
+
+    MhnParameters(m_inf, h_inf, n_inf, m_tau, h_tau, n_tau)
+end
+
+struct InversionV
+    V_Na
+    V_K
+    V_L
+end
+
+function InversionV()
+    InversionV(V_Na, V_K, V_L)
+end
+
+struct HHParams
+    conductances::Conductances
+    inversion_v::InversionV
+    I
+    C
+end
+
+function HH_standard(dyk, yk, params::HHParams, tk)
+    V, m, h, n = yk
+    (; V_Na, V_K, V_L) = params.inversion_v
+    (; g_Na, g_K, g_L) = params.conductances
+    (; m_inf, h_inf, n_inf, m_tau, h_tau, n_tau) = MhnParameters(V)
+
+    dyk[1] = (params.I - g_Na * m^3 * h * (V - V_Na) - g_K * n^4 * (V - V_K) - g_L * (V - V_L)) / params.C
+    dyk[2] = (m_inf - m) / m_tau
+    dyk[3] = (h_inf - h) / h_tau
+    dyk[4] = (n_inf - n) / n_tau
+end
+
+end
--- src/ej4/ej4.jl
+++ src/ej4/ej4.jl
@@ -1,85 +1,11 @@
+include("HH.jl")
+using .HH
+
 using Plots
 using ArgParse
 using JSON
 using DifferentialEquations
 
-default_V0 = -80
-default_I = 1
-default_tmax = 100
-g_Na = 120
-g_K = 36
-g_L = 0.3
-V_Na = 50
-V_K = -77
-V_L = -54.4
-C = 1e-6
-
-struct Conductances
-    g_Na
-    g_K
-    g_L
-end
-
-function Conductances()
-    Conductances(g_Na, g_K, g_L)
-end
-
-struct MhnParameters
-    m_inf
-    h_inf
-    n_inf
-    m_tau
-    h_tau
-    n_tau
-end
-
-function MhnParameters(V)
-    a_m = 0.1 * (V + 40) / (1 - exp((-V - 40) / 10))
-    b_m = 4 * exp((-V - 65) / 18)
-    a_h = 0.07 * exp((-V - 65) / 20)
-    b_h = 1 / (1 + exp((-V - 35) / 10))
-    a_n = 0.01 * (V + 55) / (1 - exp((-V - 55) / 10))
-    b_n = 0.125 * exp((-V - 65) / 80)
-
-    m_tau = 1 / (a_m + b_m)
-    h_tau = 1 / (a_h + b_h)
-    n_tau = 1 / (a_n + b_n)
-
-    m_inf = m_tau * a_m
-    h_inf = h_tau * a_h
-    n_inf = n_tau * a_n
-
-    MhnParameters(m_inf, h_inf, n_inf, m_tau, h_tau, n_tau)
-end
-
-struct InversionV
-    V_Na
-    V_K
-    V_L
-end
-
-function InversionV()
-    InversionV(V_Na, V_K, V_L)
-end
-
-struct ODEParams
-    conductances::Conductances
-    inversion_v::InversionV
-    I
-    C
-end
-
-function f(dyk, yk, params::ODEParams, tk)
-    V, m, h, n = yk
-    (; V_Na, V_K, V_L) = params.inversion_v
-    (; g_Na, g_K, g_L) = params.conductances
-    (; m_inf, h_inf, n_inf, m_tau, h_tau, n_tau) = MhnParameters(V)
-
-    dyk[1] = (params.I - g_Na * m^3 * h * (V - V_Na) - g_K * n^4 * (V - V_K) - g_L * (V - V_L)) / params.C
-    dyk[2] = (m_inf - m) / m_tau
-    dyk[3] = (h_inf - h) / h_tau
-    dyk[4] = (n_inf - n) / n_tau
-end
 
 function fill_defaults(cfg::Dict{String,Any})
     return Dict(
@@ -113,10 +39,10 @@ end
 function solve_system(V0, I, g, v, C, tmax)
     (; m_inf, h_inf, n_inf) = MhnParameters(V0)
     Y0 = [V0, m_inf, h_inf, n_inf]
-    params = ODEParams(g, v, I, C)
+    params = HHParams(g, v, I, C)
     tspan = (0, tmax)
 
-    prob = ODEProblem(f, Y0, tspan, params)
+    prob = ODEProblem(HH_standard, Y0, tspan, params)
     solve(prob, Rosenbrock23())
 end
 
@@ -159,8 +85,7 @@ for (i, plot_cfg) in enumerate(config["plots"])
     v = InversionV()
 
     sol = solve_system(V0, I, g, v, C, tmax)
-    PlotSolution(sol, fig_path * string(i) * ".pdf", (; V0, I))
-    plot(sol.t)
+    plot_solution(sol, fig_path * string(i) * ".pdf", (; V0, I))
 end
 
 gui()
